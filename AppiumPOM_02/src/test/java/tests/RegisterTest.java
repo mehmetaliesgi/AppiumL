1264a4c37c673c84b7d958959a774b867772849a
mehmetaliesgi <mehmetaliesgi@gmail.com>
2025-12-17 16:50:26 +0300

    Other negative tests and positive tests were written for RegisterTest.

--- AppiumPOM_02/src/test/java/tests/RegisterTest.java
+++ AppiumPOM_02/src/test/java/tests/RegisterTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.annotations.Test;
 import screens.MainScreen;
+import screens.MyAccountScreen;
 import screens.RegisterScreen;
 import utils.AlertMessages;
 
@@ -62,45 +63,188 @@ public class RegisterTest extends BaseTest{
 
     @Test
     public void invalidNameRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@")
+                .enterLastName("Esgi")
+                .enterEmail("mehmetaliesgi60@gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_INVALID_NAME);
     }
 
     @Test
     public void invalidLastNameRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("E")
+                .enterEmail("mehmetaliesgi60@gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_INVALID_LASTNAME);
     }
 
     @Test
     public void invalidEmailRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("E2")
+                .enterEmail("mehmetaliesgi60gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_INVALID_EMAIL);
     }
 
     @Test
     public void withoutNameRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("")
+                .enterLastName("Esgi")
+                .enterEmail("mehmetaliesgi60@gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_INVALID_NAME);
     }
 
     @Test
     public void withoutLastNameRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("")
+                .enterEmail("mehmetaliesgi60@gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_INVALID_LASTNAME);
     }
 
     @Test
     public void withoutEmailRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("1512")
+                .enterEmail("")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_EMPTY_EMAIL);
     }
 
     @Test
     public void withoutPasswordRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("1512")
+                .enterEmail("mehmetaliesgi60@gmail.com")
+                .enterPassword("")
+                .enterConfirmPassword("1123456790")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_EMPTY_PASSWORD);
     }
 
     @Test
     public void withoutConfirmPasswordRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("1512")
+                .enterEmail("mehmetaliesgi60gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_MISS_MATCH_PASSWORD_MESSAGE);
     }
 
     @Test
     public void withoutSecretAndSecurityPolicyRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        RegisterScreen registerScreen = (RegisterScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("@12")
+                .enterLastName("1512")
+                .enterEmail("mehmetaliesgi60gmail.com")
+                .enterPassword("1123456790")
+                .enterConfirmPassword("1123456790")
+                .clickRegister();
+
+        registerScreen.getAlertMissMatchPasswordText(alertMessages.ALERT_UNCHECKED_SECURITY_POLICY);
     }
 
     @Test
     public void withoutCampaignPolicyRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        MyAccountScreen myAccountScreen = (MyAccountScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("Test")
+                .enterLastName("test")
+                .enterEmail("YOUR_MAIL")
+                .enterPassword("YOUR_PASSWORD")
+                .enterConfirmPassword("YOUR_PASSWORD")
+                .clickSecretAndSecurityPolicy()
+                .clickRegister();
+
+        myAccountScreen.verifyMyAccountScreen("YOUR_MAIL");
     }
 
     @Test
     public void successfullyRegisterTest(){
+        MainScreen mainScreen = new MainScreen(driver);
+
+        MyAccountScreen myAccountScreen = (MyAccountScreen) mainScreen.clickMyAccount()
+                .clickRegister()
+                .enterName("Test")
+                .enterLastName("test")
+                .enterEmail("YOUR_MAIL")
+                .enterPassword("YOUR_PASSWORD")
+                .enterConfirmPassword("YOUR_PASSWORD")
+                .clickSecretAndSecurityPolicy()
+                .clickCampaignPolicy()
+                .clickRegister();
+
+        myAccountScreen.verifyMyAccountScreen("YOUR_MAIL");
     }
 }
--- AppiumPOM_02/src/test/java/utils/AlertMessages.java
+++ AppiumPOM_02/src/test/java/utils/AlertMessages.java
@@ -4,4 +4,10 @@ public class AlertMessages {
     public final String ALERT_MISS_MATCH_PASSWORD_MESSAGE = "Şifreleriniz uyuşmamaktadır. Lütfen kontrol edip tekrar deneyiniz.";
     public final String ALERT_EMAIL_ALREADY_REGISTERED = "E-Posta adresi ile daha önce kayıt olunmuştur!";
     public final String ALERT_INVALID_PASSWORD = "Şifreniz 8 ile 20 karakter arasında olmalı!";
+    public final String ALERT_INVALID_NAME = "Ad 2 ile 30 karakter arasında olmalıdır.";
+    public final String ALERT_INVALID_LASTNAME = "Soyad 2 ile 30 karakter arasında olmalıdır.";
+    public final String ALERT_INVALID_EMAIL = "E-posta adresiniz geçersizdir.";
+    public final String ALERT_EMPTY_EMAIL = "E-posta adresi 1 ile 50 karakter arasında olmalıdır.";
+    public final String ALERT_EMPTY_PASSWORD = "Giriş yaparken kullanacağınız şifrenizi giriniz.";
+    public final String ALERT_UNCHECKED_SECURITY_POLICY = "Gizlilik ve Güvenlik Politikası'nı kabul etmelisiniz.";
 }
