81b88e5faa210b92a56e3a503079253a921b8de7
mehmetaliesgi <mehmetaliesgi@gmail.com>
2025-12-18 13:28:56 +0300

    ScreenshotHelper has been created for taking screenshots.

---
+++ AppiumPOM_02/src/test/java/utils/ScreenshotHelper.java
@@ -0,0 +1,42 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ScreenshotHelper {
+    private AppiumDriver driver;
+
+    public ScreenshotHelper(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    public String takeOrganizedScreenshot(String testClassName, String testMethodName) {
+        try {
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+            String folderPath = "screenshots/" + testClassName;
+            String fileName = testMethodName + "_" + timestamp + ".png";
+            String fullPath = folderPath + "/" + fileName;
+
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File destFile = new File(fullPath);
+
+            destFile.getParentFile().mkdirs();
+            FileUtils.copyFile(srcFile, destFile);
+
+            return fullPath;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
